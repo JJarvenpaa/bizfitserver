52defc8ae4395a507029b786ce349f1f542dea7c
jarij <jari.jarvenpaa@hotmail.com>
2017-01-27 12:12:28 +0000

    viesti

--- workspace/index.js
+++ workspace/index.js
@@ -1,16 +1,18 @@
+"use strict";
 var http = require('http');
 var qs = require('querystring');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var async = require('async');
 
 var url = 'mongodb://127.0.0.1:27017/test';
 
 const PORT = 8080;
 
 
-var job="job";
-var userName="userName";
-var getSharedTrackers="getSharedTrackers";
+GLOBAL.job="job";
+GLOBAL.userName="_id";
+GLOBAL.getSharedTrackers="getSharedTrackers";
 
 
 
@@ -20,7 +22,7 @@ function handleRequest(request, response) {
     var test;
 
     
-
+   
     // TODO Better parameters for filtering out junk
     // requests to reduce workload as well as improve
     // stability and security.
@@ -47,11 +49,8 @@ function handleRequest(request, response) {
                 //body=body.replace(/\./g,'U+FF0E');
                 //body=body.replace(/$/g,'U+FF04');
 
-                
                 MongoClient.connect(url, function(err, db) {
                     assert.equal(null, err);
-
-
                     db.collection('user').save(post["user"], {
                         w: 1
                     }, function(err, result) {
@@ -73,33 +72,33 @@ function handleRequest(request, response) {
                     var end = false;
 
                     var object;
-                    var finder={'_id':post['_id']};
+                    var finder={};
+                    finder[userName]=post[userName];
                     var cursor = db.collection('user').find(finder);
                     cursor.each(function(err, doc) {
                         assert.equal(err, null);
                         if (doc != null) {
-                            var jotain={'user':doc};
                             if(post["checkSum"]!=doc["checkSum"]){
-                                object = JSON.stringify(jotain);
+                                object = JSON.stringify(doc);
 
                             }else{
                                 //object = JSON.stringify(jotain);
                             }
                             
-                        }
-                        else {
-                            object="";
-                        }
-                        if (!end) {
+                        }else{
                             response.writeHeader(200, {
                                 "Content-Type": "text/plain",
                                 //'Content-Length': object.length
                             });
-                            response.write(object);
+                            if(typeof object != 'undefined'){
+                                response.write(object);
+                            }
+                            
                             response.end();
                             end = true;
                             db.close();
                         }
+
 
                     });
                   
@@ -108,12 +107,45 @@ function handleRequest(request, response) {
             }else if(post[job]=="send_message"){
                     MongoClient.connect(url, function(err, db) {
                     assert.equal(null, err);
-
-
                     db.collection('message').save(post["message"], {
                         w: 1
                     }, function(err, result) {
-                        db.close();
+                        var finder={};
+                        finder[userName] = post['message']['resipient'];
+                        var cursor = db.collection('user').find(finder);
+                        cursor.each(function(err, user) 
+                        {
+                            assert.equal(err, null);
+                            if (user != null) 
+                            {
+                                //conversations=user.conversations;
+                                var conversationFound=false;
+                                var conversations= user['conversations'];
+                                for(var i=0; i < conversations.length&&!conversationFound;i++)
+                                {
+                                    var conversation=conversations[i];
+                                    if(conversation['other'] == post['message']['sender'])
+                                    {
+                                        conversationFound = true;
+                                        
+                                    }
+                                }
+                                if(conversationFound===false)
+                                {
+                                    
+                                    var conversation = {'owner': post['message']['resipient'],'other': post['message']['sender']};
+                                    user['conversations'].push(conversation);
+                                    db.collection('user').save(user,{
+                                        w:1
+                                    },function(err, result){
+                                        db.close();
+                                    });
+                                }
+                                
+                            }
+                            
+                        });
+                       
                     });
                     response.writeHeader(200, {
                         "Content-Type": "text/plain",
@@ -126,34 +158,39 @@ function handleRequest(request, response) {
                 });
             }else if(post[job]=="get_message_incoming"){
                 MongoClient.connect(url, function(err, db) {
+                    console.log("prööt");
                     assert.equal(null, err);
                     var object;
-                    var array=[];
+                    var messageArray=[];
                     //TODO haku toistenpäin
                     //var finder={function() { return (this.resipient == post['owner'] && this.sender==post['other'] ) } };
                     //var finder={'&and':[{'resipient':{'&eq' :post['owner']}},{'sender':{'&eq' :post['other']}}]};
                     var finder={'resipient':post['owner']};
+                    console.log(finder);
                     var cursor = db.collection('message').find(finder);
                     cursor.each(function(err, doc) {
+                        console.log(doc);
                         assert.equal(err, null);
                         if (doc != null) {
                             //var jotain={'user':doc};
+                            console.log(post["other"]);
+                            console.log(doc["sender"])
                             if(post["creationTime"]<doc["creationTime"] && post["other"] == doc["sender"]){
-                                array.push(JSON.stringify(doc));
+                                messageArray.push(JSON.stringify(doc));
                             }else{
                                 //array.push(JSON.stringify(doc)); // for testing only
                             }
                         }
                         else 
-                        {                        
-                                    var text=JSON.stringify(array);
+                        {                  
+                        console.log(messageArray);
+                                    var text=JSON.stringify(messageArray);
                                      response.writeHeader(200, {
                                     "Content-Type": "text/plain",
                                     //'Content-Length': text.length
                                     });
                                     response.write(text)
                                     response.end();
-                                    end = true;
                                     db.close();     
                            
                        
@@ -193,7 +230,6 @@ function handleRequest(request, response) {
                                     });
                                     response.write(text)
                                     response.end();
-                                    end = true;
                                     db.close();     
                            
                        
@@ -209,7 +245,8 @@ function handleRequest(request, response) {
                 
                 MongoClient.connect(url,function(err,db){
                     var newConversation=post['conversation'];
-                    var finder={'_id':newConversation['owner']};
+                    var finder={};
+                    finder[userName]=newConversation['owner'];
                     var cursor=db.collection('user').find(finder);
                     var end=false;
                     cursor.each(function(err, user) {
@@ -259,7 +296,8 @@ function handleRequest(request, response) {
             {
                 MongoClient.connect(url,function(err,db){
                     
-                    var finder={'_id':post[userName]};
+                    var finder={};
+                    finder[userName]=post[userName];
                     var newTracker = post['Tracker'];
                     var cursor=db.collection('user').find(finder);
                     cursor.each(function(err, user) 
@@ -317,40 +355,77 @@ function handleRequest(request, response) {
             }
              else if(post[job]==getSharedTrackers)
             {
+                //lisää async
                 var jokuLista=[];
                 MongoClient.connect(url,function(err,db)
                 {
                     var list=post["list"];
-                    for(var i=0;i<list.length;i++)
-                    {
-                        var sharedTracker=list[i];
-                        var finder={'_id':post[userName]};
-                        var cursor=db.collection('user').find(finder);
-                        cursor.each(function(err, user) 
-                        {
-                            var trackers=user['trackers'];
-                            if(trackers!=null)
-                            {
-                                for(var j=0;j<trackers.length;j++)
-                                {
-                                    var tracker=trackers[i];
-                                    if(tracker['creationTime']==sharedTracker['creationTime'])
+                    async.each(list,function(name,callback){
+                        var sharedTracker=name;
+                        var finder={};
+                        finder[userName]=name[userName];
+                        if(typeof finder[userName] != 'undefined'){
+
+                            var cursor=db.collection('user').find(finder);
+                            cursor.nextObject(function fn(err, user) {
+                                if (err || !user){
+                                    callback();
+                                    return;
+                                } 
+                            
+                                setImmediate(fnAction, user, function() {
+                                    cursor.nextObject(fn);
+                                    
+                                });
+                            });
+                            
+  
+                            
+                            function fnAction(user, callback) {
+                                // Here you can do whatever you want to do with your item.
+                                if(typeof user!= 'undefined'&& user!=null && user['trackers']!= 'undefined'){
+                                    var trackers=user['trackers'];
+                                    if(trackers!=null)
                                     {
-                                        jokuLista.push(JSON.stringify(tracker));
+                                        for(var j=0;j<trackers.length;j++)
+                                        {
+                                            var tracker=trackers[j];
+                                            if(tracker['creationTime']==sharedTracker['creationTime'])
+                                            {
+                                                jokuLista.push(JSON.stringify(tracker));
+                                            }
+                                        }
                                     }
                                 }
+                                return callback();
                             }
-                        });
-                    }
-                    response.writeHeader(200, 
+                            
+                            
+                           
+                        }
+                        
+                    },function(err){
+                        if(err){
+                            response.writeHeader(200, 
+                            {
+                                "Content-Type": "text/plain",
+                                //'Content-Length': object.length
+                            });
+                            response.write("failed");
+                            //db.close();
+                            return err;
+                        }
+                        response.writeHeader(200, 
                         {
                             "Content-Type": "text/plain",
                             //'Content-Length': object.length
                         });
-                    response.write(JSON.stringify(jokuLista));
-                    db.close();
-                    response.end();
-                   
+                       
+                        response.write(JSON.stringify(jokuLista))
+                        db.close();
+                        response.end();
+                    });
+                  
                 });
             }
             
